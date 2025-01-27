085e0aa738166d0ddd01feb1ebb25dc82fce94d8
Red <tolentinored19@gmail.com>
2025-01-27 20:00:42 +0800

    jsdsa

--- 4bigo-constant-o1.js
+++ 4bigo-constant-o1.js
@@ -1,20 +1,19 @@
 //01 - "constant time" regardless of the number of inputs
 //example - you know exactly where to find an item - no matter if the input grows or shrinks
 
-const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+// ----
+// const findNumberTwo = (number) => {
+// 	const numberFound = numbers[0];
 
-const findNumberTwo = (number) => {
-	const numberFound = numbers[0];
+// 	if (numberFound === number) {
+// 		return true;
+// 	}
 
-	if (numberFound === number) {
-		return true;
-	}
+// 	return false;
+// };
 
-	return false;
-};
-
-console.log(findNumberTwo(2)); //true
-console.log(findNumberTwo(100)); //false
+// console.log(findNumberTwo(2)); //true
+// console.log(findNumberTwo(100)); //false
 
 //01 - finding a value without loops
 
---
+++ index.js
@@ -0,0 +1,3 @@
+const numbers = [2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+const getElement = (arr, index) => arr[index];
